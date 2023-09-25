28f499731fe7159a7086ce4797f740ea968ab5ef
Fin <uk059783@student.uni-kassel.de>
2023-09-25 09:01:18 +0200

    feat: extended CardDaoTest to test all cardDao functions

--- app/src/androidTest/java/com/example/indexcardtrainer/core/data/database/CardDaoTest.kt
+++ app/src/androidTest/java/com/example/indexcardtrainer/core/data/database/CardDaoTest.kt
@@ -12,6 +12,7 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -39,8 +40,10 @@ class CardDaoTest {
     }
 
     @Test
-    fun upsertIndexCard() = runTest {
+    fun getAllIndexCards() = runTest {
+        var cards : List<IndexCard> = emptyList()
         val indexCard = IndexCard(
+            id = 1,
             title = "Test",
             solution = "test",
             category = "Test"
@@ -48,12 +51,152 @@ class CardDaoTest {
         async {
             cardDao.upsertIndexCard(indexCard)
         }.await()
-        var cards : List<IndexCard> = emptyList()
         async {
             cards = cardDao.getAllIndexCards()
         }.await()
-        println(">>>>>>>>>>>>>>>> $cards")
-        //assertEquals(cards[0], indexCard)
-        //assertTrue(cards.contains(indexCard))
+        assertEquals(listOf(indexCard), cards)
+    }
+
+    @Test
+    fun upsertIndexCard() = runTest {
+        var cards : List<IndexCard> = emptyList()
+        val indexCard = IndexCard(
+            id = 1,
+            title = "Test",
+            solution = "test",
+            category = "Test"
+        )
+        async {
+            cards = cardDao.getAllIndexCards()
+        }.await()
+        assertFalse(cards.contains(indexCard))
+
+        // Insert
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            cards = cardDao.getAllIndexCards()
+        }.await()
+        assertEquals(cards[0], indexCard)
+
+        // Update
+        indexCard.apply {
+            title = "Test2"
+            solution = "test2"
+            category = "Test2"
+        }
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            cards = cardDao.getAllIndexCards()
+        }.await()
+        assertEquals(cards[0].title, "Test2")
+        assertEquals(cards[0].solution, "test2")
+        assertEquals(cards[0].category, "Test2")
+    }
+
+    @Test
+    fun deleteIndexCard() = runTest {
+        var cards : List<IndexCard> = emptyList()
+        val indexCard = IndexCard(
+            id = 1,
+            title = "Test",
+            solution = "test",
+            category = "Test"
+        )
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            cards = cardDao.getAllIndexCards()
+        }.await()
+        assertTrue(cards.contains(indexCard))
+
+        // Delete
+        async {
+            cardDao.deleteIndexCard(indexCard)
+        }.await()
+        async {
+            cards = cardDao.getAllIndexCards()
+        }.await()
+        assertFalse(cards.contains(indexCard))
+    }
+
+    @Test
+    fun getRecentlyFailedCards() = runTest {
+        var recentlyFailedCards : List<IndexCard> = emptyList()
+        val indexCard = IndexCard(
+            id = 1,
+            title = "Test",
+            solution = "test",
+            category = "Test",
+            isRecentlyFailed = true
+        )
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            recentlyFailedCards = cardDao.getRecentlyFailedCards()
+        }.await()
+        assertTrue(recentlyFailedCards.contains(indexCard))
+    }
+
+    @Test
+    fun getIndexCardById() = runTest {
+        var cardById : IndexCard? = null
+        val indexCard = IndexCard(
+            id = 1,
+            title = "Test",
+            solution = "test",
+            category = "Test",
+            isRecentlyFailed = true
+        )
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            cardById = cardDao.getIndexCardById(1)
+        }.await()
+        assertEquals(indexCard, cardById)
+    }
+
+    @Test
+    fun getIndexCardsWithCategory() = runTest {
+        var cardsByCategory : List<IndexCard> = emptyList()
+        val indexCard = IndexCard(
+            id = 1,
+            title = "Test",
+            solution = "test",
+            category = "Test",
+            isRecentlyFailed = true
+        )
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            cardsByCategory = cardDao.getIndexCardsWithCategory("Test")
+        }.await()
+        assertTrue(cardsByCategory.contains(indexCard))
+    }
+
+    @Test
+    fun getAllCategories() = runTest {
+        var categories : List<String> = emptyList()
+        val indexCard = IndexCard(
+            id = 1,
+            title = "Test",
+            solution = "test",
+            category = "Test",
+            isRecentlyFailed = true
+        )
+        async {
+            cardDao.upsertIndexCard(indexCard)
+        }.await()
+        async {
+            categories = cardDao.getAllCategories()
+        }.await()
+        assertEquals(listOf("Test"), categories)
     }
 }
