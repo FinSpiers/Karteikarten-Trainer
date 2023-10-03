45faefc423052f3d00954edcd7d0046a265f432c
Fin <uk059783@student.uni-kassel.de>
2023-10-03 12:23:33 +0200

    feat: Added format function to print formatted timestamps

--- app/src/main/java/com/example/indexcardtrainer/core/domain/util/DateTimeConverter.kt
+++ app/src/main/java/com/example/indexcardtrainer/core/domain/util/DateTimeConverter.kt
@@ -8,4 +8,18 @@ object DateTimeConverter {
     fun parseDateTime(timestamp : Long) : LocalDateTime {
         return LocalDateTime.ofInstant(Instant.ofEpochSecond(timestamp), TimeZone.getDefault().toZoneId())
     }
+
+    fun formatRuntime(runtimeSeconds: Long) : String {
+        val minutes = runtimeSeconds / 60
+        return if (minutes > 0) {
+            val hours = minutes / 60
+            if (hours > 0) {
+                "${(hours).toString().padStart(2, '0')}:${(minutes / 60).toString().padStart(2, '0')}:${(runtimeSeconds % 60).toString().padStart(2, '0')} h"
+            } else {
+                "${(minutes).toString().padStart(2, '0')}:${(runtimeSeconds % 60).toString().padStart(2, '0')} min"
+            }
+        } else {
+            "$runtimeSeconds sec"
+        }
+    }
 }
